8a84288d2e194b5ce2b8a562fee077ef4f509cf1
kaan <kaanhitay@hotmail.com>
2026-01-21 00:48:24 +0300

    Deep dive into Furniture sub-categories; identified loss in Tables and Bookcases

--- sql_queries/02_exploratory_analysis.sql
+++ sql_queries/02_exploratory_analysis.sql
@@ -19,3 +19,16 @@ SELECT
 FROM superstore_sales
 GROUP BY category
 ORDER BY total_profit DESC;
+
+-- Query 2: Deep Dive into Furniture Sub-Categories
+-- Objective: Find out which sub-categories are dragging down the Furniture category's profit.
+
+SELECT 
+    sub_category, 
+    ROUND(SUM(sales), 2) AS total_sales, 
+    ROUND(SUM(profit), 2) AS total_profit,
+    ROUND(AVG(discount) * 100, 2) AS avg_discount_percent
+FROM superstore_sales
+WHERE category = 'Furniture'
+GROUP BY sub_category
+ORDER BY total_profit ASC;
